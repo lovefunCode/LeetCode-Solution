4e15f8e22daa9d2d05ae780aa468e745b1b6fbd3
fang01.liu <fang01.liu@meditrusthealth.com>
2024-10-28 12:39:30 +0000

    change comment 'way' to 'solution'

--- createTwoDimensionalArray.js
+++ createTwoDimensionalArray.js
@@ -1,14 +1,14 @@
-// create 2D matrix ways
+// create 2D matrix solutions
 // because in JS, there is no built-in method to create a 2D matrix.
 
 const rows = 10;
 const columns = 10;
 
 
-//way1:  directly assign 
+//solution 1:  directly assign 
 const myArray = [[],[],[],[],[],[],[]]
 
-// way 2:
+// solution 2:
 const myArray2 = []
 for(let i = 0; i < rows; i++){
     myArray2[i] = []
@@ -19,13 +19,13 @@ for(let i = 0; i < rows; i++){
 
 console.log(myArray2)
 
-//way 3:
+//solution 3:
 const myArray3 = new Array(rows)
 for(let i = 0; i < columns; i++){
     myArray3[i] = new Array(columns).fill(null)
 }
 console.log(myArray3)
 
-//way 4:
+//solution 4:
 const myArray4 = Array.from(Array(rows), ()=>new Array(columns).fill(1))
 console.log(myArray4)
