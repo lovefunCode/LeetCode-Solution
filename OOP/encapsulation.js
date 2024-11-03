8eaf288d0debd1688c3d50590be3bdd583e91c00
fang01.liu <fang01.liu@meditrusthealth.com>
2024-11-03 21:36:51 +0000

    add note static method  in class in  encapsulation.js file

--- OOP/encapsulation.js
+++ OOP/encapsulation.js
@@ -11,10 +11,20 @@ class Car{
     getEngineStatus(){
         return this.#engineStatus
     }
+    // static
+    static engineMethod(){
+        console.log('I am a static method in a class')
+    }
 }
 
 const myCar = new Car('Toyota')
 myCar.startEngine();    //Start engine
 console.log(myCar.getEngineStatus())  // on
 console.log(myCar.make)    //Toyota 
-console.log(myCar.#engineStatus) //SyntaxError: Private field '#engineStatus' must be declared in an enclosing class
+// console.log(myCar.#engineStatus) //SyntaxError: Private field '#engineStatus' must be declared in an enclosing class
+
+//Static method need use class name to call 
+Car.engineMethod()  //I am a static method in a class
+
+// Note: cannot use instance to call the class static method
+myCar.engineMethod(); //TypeError: myCar.engineMethod is not a function
