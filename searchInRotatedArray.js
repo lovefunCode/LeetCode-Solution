cf3a1ffbd91351e7da3331bd7c23f7e8b279e7b7
fang01.liu <fang01.liu@meditrusthealth.com>
2024-11-06 15:55:57 +0000

    add searchInRotatedArray  function

---
+++ searchInRotatedArray.js
@@ -0,0 +1,26 @@
+
+const searchInRotatedAttay = function(nums, target){
+    let left = 0;
+    let right = nums.length;
+    while(left <= right){
+        let middle = left+ Math.floor(right-left)
+        if(nums[middle] == target) return middle
+        if(nums[middle] > nums[left]){
+            if(target < nums[middle] && target >nums[middle]){
+                right = middle-1
+            }else{
+                left = middle + 1
+            }
+        }else{
+            if(target < nums[right] && target > nums[middle]){
+                left = middle+1
+            }else{
+                right = middle -1
+            }
+        }
+    }
+    return -1
+}
+
+const nums = [4,5,6,7,0,1,2], target = 0
+console.log(searchInRotatedAttay(nums, target))
