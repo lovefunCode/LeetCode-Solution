f02966686efb0519e636b5283d8e5c05161bf778
fang01.liu <fang01.liu@meditrusthealth.com>
2024-11-08 10:57:12 +0000

    add twoSumWithTwoPointer in twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -1,3 +1,4 @@
+// Map
 const twoSum = function(nums, target){
     const map = new Map()
     for(let i = 0; i < nums.length; i++){
@@ -9,6 +10,26 @@ const twoSum = function(nums, target){
         }
     }
 }
+
+// Two-Pointer need ordered array
+const twoSumWithTwoPointer = function(nums, target){
+    let left = 0;
+    let right = nums.length - 1;
+    while(left < right){
+        console.log(nums[left] + nums[right])
+        if(nums[left] + nums[right] == target){
+            return [left, right]
+        }else if(nums[left] + nums[right] > target){
+            right--
+        }else{
+            left++
+        }
+    }
+    return -1
+}
+
+
 // const nums = [2,7,11,15], target = 9
-const nums = [3,2,4], target = 6
+const nums = [2,3,4], target = 6
 console.log(twoSum(nums, target))
+console.log(twoSumWithTwoPointer(nums, target))
