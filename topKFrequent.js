b59437c8f8bfc35e7b717ab28b6aedc0482dbda0
fang01.liu <fang01.liu@meditrusthealth.com>
2024-11-10 16:12:09 +0000

    add different loop Object method in topKFrequent function

--- topKFrequent.js
+++ topKFrequent.js
@@ -8,18 +8,24 @@ const topKFrequent = function(nums, k){
             map[nums[i]]++
         }
     }
-    console.log('map---', map)
-    for(let [key, value] of Object.entries(map)){
-        console.log(key, value)
-        if(key >= k){
-            result.push(value)
+    // console.log('map---', map)
+    // for(let [key, value] of Object.entries(map)){
+    //     console.log(key, value)
+    //     if(key >= k){
+    //         result.push(value)
+    //     }
+    // }
+    Object.keys(map).map(item=>{
+        console.log(item)
+        if(item >= k){
+            result.push(map[item])
         }
-    }
+    })
     return result
 }
 
 const nums = [1,1,1,2,2,3], k = 2
-console.log(topKFrequent(nums, k))
+console.log('result---', topKFrequent(nums, k))
 
 const nums2 = [1], k2 = 1
-console.log(topKFrequent(nums2, k2))
+// console.log(topKFrequent(nums2, k2))
