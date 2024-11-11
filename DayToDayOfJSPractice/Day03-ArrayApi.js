a1a8767c42b66d1a700c7ecb7776703367ab320f
fang01.liu <fang01.liu@meditrusthealth.com>
2024-11-11 17:13:34 +0000

    add part of Day03-Array.js

--- DayToDayOfJSPractice/Day03-ArrayApi.js
+++ DayToDayOfJSPractice/Day03-ArrayApi.js
@@ -1,9 +1,36 @@
 //higher-order function
 // Pure function, higher-order function, 
-// map, filter, reduce, forEach
+// map, filter, reduce, sometimes forEach can 
 
 //side effect,
+//  change the original array
 // sort, forEach
+const nums = [3, 4, 5, 7, 8, 0]
+nums.sort((a, b) => a -b)
+console.log(nums)    //[ 0, 3, 4, 5, 7, 8 ]
+
+let newNums =[]
+const returnResult = nums.forEach((val, index)=>{
+    if(index % 2 == 0){
+        newNums.push(val*2)  //This is a side effect; it modifies the external variable `newNums`
+        return val * 2
+    }
+    return newNums.push(val)
+})
+
+console.log(nums)   //[ 0, 3, 4, 5, 7, 8 ]
+// Note: forEach don't return a new array, return undefined
+console.log('newNums----',newNums)  // newNums---- [ 0, 3, 8, 5, 14, 8 ]
+console.log('returnResult----', returnResult)   //returnResult---- undefined
+
+nums.forEach((val, index)=>{
+    if(index % 2 == 0){
+        nums[index] = nums[index] * 2     //This is a side effect; it modifies the original nums
+    }
+    
+})
+// Note: use foreach, if use the index to change nums, it will cause side effect
+console.log(nums)   //[ 0, 3, 8, 5, 14, 8 ]
 
 
 
