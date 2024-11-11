151a33ff427b79af07de4b758c231911b9829f49
fang01.liu <fang01.liu@meditrusthealth.com>
2024-11-11 15:16:47 +0000

    add class syntax write LRU cache

--- LeastRencentlyUsedCache.js
+++ LeastRencentlyUsedCache.js
@@ -36,11 +36,55 @@ LRUCache.prototype.get = function(key){
     }
 }
 
+
+class LRU{
+    constructor(capacity){
+        this.capacity = capacity
+        this.cache = new Map()
+    }
+    put(key, val){
+        if(this.cache.size == this.capacity){
+            if(this.cache.has(key)){
+                this.cache.delete(key)
+                // this.cache.set(key, val)
+            }else{
+                for(let keyVal of this.cache){
+                    // Evict the first (least frequently used)
+                    this.cache.delete(keyVal[0])
+                    break;
+                }
+                // this.cache.set(key, val)
+            }
+        }
+        this.cache.set(key, val)
+    }
+    get(key){
+        if(this.cache.has(key)){
+            let val = this.cache.get(key)
+            this.cache.delete(key)
+            this.cache.set(key, val)
+            return val
+        }else{
+            return -1
+        }
+    }
+}
+
+
 const obj = new LRUCache(2)
 obj.put('a', [1,2,3])
 obj.put('b', [2,3,4])
 obj.put('c', 3)
 
-console.log("obj.get(1)=====", obj.get('a'));  // Expected output: -1 (1 was evicted)
-console.log("obj.get(2)", obj.get('b'));  // Expected output: [2, 3, 4] (2 is still in the cache)
-console.log("obj.get(3)", obj.get('c'));  // Expected output: 3 (3 is in the cache)
+console.log("obj.get(a)=====", obj.get('a'));  // Expected output: -1 (1 was evicted)
+console.log("obj.get(b)", obj.get('b'));  // Expected output: [2, 3, 4] (2 is still in the cache)
+console.log("obj.get(c)", obj.get('c'));  // Expected output: 3 (3 is in the cache)
+
+const obj2 = new LRU(2)
+obj2.put('a', [1,2,3])
+obj2.put('b', [2,3,4])
+obj2.put('c', 3)
+console.log("=============")
+console.log("obj2.get(a)=====", obj2.get('a'));  // Expected output: -1 (1 was evicted)
+console.log("obj.get(b)", obj2.get('b'));  // Expected output: [2, 3, 4] (2 is still in the cache)
+console.log("obj.get(c)", obj2.get('c'));  // Expected output: 3 (3 is in the cache)
