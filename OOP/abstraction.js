29048905b45dafa351cf0c9c0d68c2484f7bee14
fang01.liu <fang01.liu@meditrusthealth.com>
2025-06-09 16:47:46 +0100

    optimize OOP abstraction.js

--- OOP/abstraction.js
+++ OOP/abstraction.js
@@ -8,11 +8,16 @@ class Car {
     startCar() {
         this._startIgnition();
         console.log(`${this.make} ${this.model} is starting.`);
+        console.log("=======")
+        this.#privateMethod()
     }
 
     // Private method, hidden from the outside and used only internally
     _startIgnition() {
         console.log('Ignition started');
+    }
+    #privateMethod(){
+        console.log("Is this a private method?")
     }
 }
 
@@ -20,5 +25,8 @@ const myCar = new Car('Toyota', 'Corolla');
 myCar.startCar(); // Output: Ignition started
                   //         Toyota Corolla is starting
 
-// The following line will throw an error or be discouraged as _startIgnition is internal:
-// myCar._startIgnition(); // Not meant to be accessed directly
+// The following line will not be discouraged as _startIgnition is internal:
+myCar._startIgnition(); // Not meant to be accessed directly
+
+// SyntaxError: Private field '#privateMethod' must be declared in an enclosing class
+// myCar.#privateMethod();
